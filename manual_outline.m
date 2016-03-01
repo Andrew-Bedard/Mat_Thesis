e1cd92c2c6217f868458306b2869732170f2e77d
Andrew Bedard <andrew.bedard@student.uva.nl>
2016-03-02 00:08:16 +0100

    Added manual_outline which allows the selection of a polygon manually from image

---
+++ manual_outline.m
@@ -0,0 +1,11 @@
+
+%Allows one to select outline of image and output it as a collection of
+%coordinates
+
+I = imread('Fox_late-gastrula2.jpg');
+I = rgb2gray(I)
+figure, imshow(I)
+BW = roipoly(I)
+
+%Now requires the selection of the inside of the polygon to copy and paste
+%into workspace
