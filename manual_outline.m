682177f2b9faabd8d2f83b53cb78b8e656c00c82
Andrew Bedard <andrew.bedard@student.uva.nl>
2016-03-05 21:31:03 +0100

    Changed Fox_late image, method is outlined in manual_outline

--- manual_outline.m
+++ manual_outline.m
@@ -8,5 +8,8 @@ figure, imshow(I)
 BW = roipoly(I);
 
 %Now requires the selection of the inside of the polygon to create a mask
-%with, then use the following: BW2 = bwperim(BW,8);
+%with, then use the following: BW2 = bwperim(BW,8); BW3 = imdilate(BW2,
+%strel('disk',1));
+%where imdilate increases the edge thickness because manual outlines are
+%not going to be perfect
 
