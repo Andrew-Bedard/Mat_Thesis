01e8253a18562a565c2fa9afe4abd6cf0844e447
Andrew Bedard <andrew.bedard@student.uva.nl>
2016-08-03 23:33:12 +0200

    more stuff with narrowing down analysis to the effective range

--- New_stuff/Exclude_stages/Gast_Stage.m
+++ New_stuff/Exclude_stages/Gast_Stage.m
@@ -1,4 +1,4 @@
-function [bool_range, hpf_range] = Gast_Stage(Im_name, Dev_stages, effective_index)
+function [bool_range, hpf_range, dummy_index, dummy_lengths, longest_index] = Gast_Stage(Im_name, Dev_stages, effective_index)
 
 %Searches image name for information regarding the stage of gastrulation,
 %if the stage labelled in the image name is within the effective range,
@@ -8,14 +8,36 @@ function [bool_range, hpf_range] = Gast_Stage(Im_name, Dev_stages, effective_ind
 %of gastrulation where our algorithm is effective
 
 hpf_range = [0,0];
-abrevs = Dev_stages.abrevs;
+dummy_index = zeros(1,20);
+dummy_lengths = zeros(1,20);
+counter = 0;
 
-for i = 1:length(abrevs)
-    stage = abrevs{i};
-    for j = 1:length(abrevs{i})
+for i = 1:length(effective_index)
+    
+    stage = Dev_stages(effective_index(i)).abrevs;
+    
+    for j = 1:length(stage)
+        
+        %Compare strings
         k = strfind(Im_name, stage(j));
+        
+        %If there is a positive match, save some values
         if isempty(k) == false
-            stage(j);
+            counter = counter + 1;
+            %Save the index of effective_index where postiive value found
+            dummy_index(counter) = i;
+            %Save the length of the abreviation found, as there is overlap
+            %in the naming convention we will default to using the longer
+            %name as it provides greater specificity
+            dummy_lengths(counter) = length(stage{j});
+            %bool_range indicates we have found a match
+            bool_range = true;
+            
         end
     end
 end
+
+dummy_index = dummy_index(dummy_index ~= 0);
+dummy_lengths = dummy_lengths(dummy_lengths ~= 0);
+
+longest_index = find(max(dummy_lengths));
